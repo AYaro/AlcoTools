68772f178c28cc8820bb8ebac2e91a6bf50da6f4
Alexandr Yaroshevich <0370384@mail.ru>
2021-01-07 21:31:56 +0300

    :bomb: update home fragment

--- app/src/main/java/com/example/alcotools/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/alcotools/ui/home/HomeFragment.kt
@@ -5,10 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
 import com.example.alcotools.R
+import com.example.alcotools.ui.drinksList.DataStorage
+import com.example.alcotools.ui.drinksList.DrinkAdapter
 
 class HomeFragment : Fragment() {
 
@@ -27,5 +32,17 @@ class HomeFragment : Fragment() {
             textView.text = it
         })
         return root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val drinks = DataStorage.getDrinksList()
+        val adapter = DrinkTextAdapter(this.requireContext(), drinks)
+        val list = this.activity?.findViewById<RecyclerView>(R.id.recycler)
+        list?.adapter = adapter
+        val decoration = DividerItemDecoration(this.requireContext(), DividerItemDecoration.HORIZONTAL)
+        decoration.setDrawable(ContextCompat.getDrawable(this.requireContext(), R.color.purple_700)!!)
+        list?.addItemDecoration(decoration)
+        // todo onclick intent add toggle button
     }
 }
