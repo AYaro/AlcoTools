677b138f0d69dd0e612eeb3eb4a81e9d293980ef
dunaevai135 <dunaevai13579@gmail.com>
2021-01-18 13:13:26 +0300

    add progressBar move by drunk button

--- app/src/main/java/com/example/alcotools/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/alcotools/ui/home/HomeFragment.kt
@@ -1,10 +1,15 @@
 package com.example.alcotools.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatImageButton
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -43,5 +48,14 @@ class HomeFragment : Fragment() {
         decoration.setDrawable(ContextCompat.getDrawable(this.requireContext(), R.color.purple_700)!!)
         list?.addItemDecoration(decoration)
         // todo onclick intent add toggle button
+        val home_drink_button = this.activity?.findViewById<AppCompatImageButton>(R.id.home_drink_button)
+        val progressBar = this.requireActivity().findViewById<ProgressBar>(R.id.progressBar)
+        home_drink_button?.setOnClickListener { v: View? -> run {
+            progressBar.progress = progressBar.progress + 5;
+
+            if (progressBar.progress >= 100) {
+                Toast.makeText(this.requireContext(), "You drunk as f@%k", Toast.LENGTH_SHORT).show()
+            }
+        } }
     }
 }
